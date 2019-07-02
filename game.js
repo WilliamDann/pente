55f4a3b1cf7891282eed94e2372dd670a0961b05
WilliamDann <wcdann1@gmail.com>
2019-07-02 11:11:43 -0700

    inital commit

---
+++ game.js
@@ -0,0 +1,66 @@
+const BOARD_SIZE = 32
+
+// Class to draw a pente board
+class Board {
+    constructor(game, canvas) {
+        this.canvas = canvas
+        this.game = game;
+
+        document.addEventListener('click', e => {
+            if (!e.target.matches('#gameCanvas')) return;
+            
+            this.click(e.clientX, e.clientY); // TODO allow canvas to be offset
+        });
+    }
+    
+    // draw a pente board
+    update() {
+        let ctx = this.canvas.getContext('2d');
+        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        let x = 1, y = 1;
+        for (let dataX of this.game.board) {
+            for (let dataY of dataX) {
+                ctx.beginPath();
+                ctx.strokeStyle = 'black';
+                ctx.rect(x*BOARD_SIZE, y*BOARD_SIZE, BOARD_SIZE, BOARD_SIZE);
+                ctx.stroke();
+                
+                if (dataY == 'a') {
+                    ctx.beginPath();
+                    ctx.strokeStyle = 'red';
+                    ctx.fillStyle = 'red';
+                    ctx.arc(x*BOARD_SIZE, y*BOARD_SIZE, 10, 0, 2*Math.PI);
+                    ctx.fill();                    
+                    ctx.stroke();                
+                } else if (dataY == 'b') {
+                    ctx.beginPath();
+                    ctx.strokeStyle = 'blue';
+                    ctx.fillStyle = 'blue';                    
+                    ctx.arc(x*BOARD_SIZE, y*BOARD_SIZE, 10, 0, 2*Math.PI);
+                    ctx.fill();
+                    ctx.stroke();
+                }
+                
+                x++;
+            }
+            y++;
+            x = 1;
+        }
+
+        ctx.strokeStyle = 'grey';
+        ctx.moveTo(BOARD_SIZE * 10, BOARD_SIZE * 10);
+        ctx.arc(BOARD_SIZE * 10, BOARD_SIZE * 10, 10, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
+
+    // Handle a click at a point
+    click(xPos, yPos) {
+        this.game.place(Math.floor((xPos - (BOARD_SIZE / 2)) / BOARD_SIZE), Math.floor((yPos - (BOARD_SIZE / 2)) / BOARD_SIZE));
+
+        this.update();
+    }
+}
+
+let board = new Board(new Pente(), document.getElementById('gameCanvas'));
+board.update();
---
+++ pente.js
@@ -0,0 +1,93 @@
+const BLANK_GAME = "18/18/18/18/18/18/18/18/18/18/18/18/18/18/18/18/18/18/"
+const CAPTURE_TABLE = [
+    [{x:0, y:0, type:'a'}, {x:0, y:1, type:'b'},  {x:0, y:2, type:'b'},  {x:0, y:3, type:'a'}],
+    [{x:0, y:0, type:'a'}, {x:0, y:-1, type:'b'}, {x:0, y:-2, type:'b'}, {x:0, y:-3, type:'a'}],
+    [{x:0, y:0, type:'a'}, {x:1, y:0, type:'b'},  {x:2, y:0, type:'b'},  {x:3, y:0, type:'a'}],
+    [{x:0, y:0, type:'a'}, {x:-1, y:0, type:'b'}, {x:-2, y:0, type:'b'}, {x:-3, y:0, type:'a'}],
+
+    [{x:0, y:0, type:'a'}, {x:1, y:1, type:'b'},  {x:2, y:2, type:'b'}, {x:3, y:3, type:'a'}],
+    [{x:0, y:0, type:'a'}, {x:-1, y:1, type:'b'}, {x:-2, y:2, type:'b'}, {x:-3, y:3, type:'a'}],
+    [{x:0, y:0, type:'a'}, {x:1, y:-1, type:'b'}, {x:2, y:-2, type:'b'}, {x:3, y:-3, type:'a'}],
+    [{x:0, y:0, type:'a'}, {x:-1, y:-1, type:'b'}, {x:-2, y:-2, type:'b'}, {x:-3, y:-3, type:'a'}],
+    [{x:0, y:0, type:'a'}, {x:1, y:1, type:'b'}, {x:2, y:2, type:'b'}, {x:3, y:3, type:'a'}],
+
+]
+// Class to store board information
+class Pente {
+    constructor(fen=BLANK_GAME) {
+        this.board = this.create(fen);
+        this.a = true;
+    }
+
+    // Create the board from fen notation
+    create(fen) {
+        let board = []
+
+        for (let i of fen.split('/')) {
+            let row = []
+            for (let char of i.split('-')) {
+                if (char == "") continue;
+
+                let num = parseInt(char);
+                if (num) {
+                    for (let i = 0; i < num; i++) {
+                        row.push('0')
+                    }
+                } else { row.push(char); }
+            }
+
+            if (row.length != 0) board.push(row);
+        }
+
+        return board;
+    }
+
+    // Place a piece on the board
+    place(x, y) {
+        if (this.board[y][x] != '0') return;
+
+        (this.a) ? this.board[y][x] = 'a' : this.board[y][x] = 'b'
+        this.captureCheck(x, y);
+
+        this.a = !this.a;
+    }
+
+    // Check the board state
+    captureCheck(x, y) {
+        for (let pattern of CAPTURE_TABLE) {
+            let capture = true;
+
+            let expected = this.a;
+
+            // check for captures
+            for (let space of pattern) {
+                expected = (this.a) ? ((space.type == 'a') ? 'a': 'b') : (space.type == 'a') ? 'b' : 'a';
+
+                if (this.board[y+space.y][x+space.x] != expected) {
+                    capture = false;
+                    break;
+                }
+                else
+                    capture = true;
+            }
+
+            // capture
+            if (capture) {
+                for (let space of pattern) {
+                    this.board[y+space.y][x+space.x] = (this.a) ? ((space.type == 'a') ? 'a' : '0') : ((space.type == 'a' ? 'b' : '0'))
+                }
+            }
+        }
+    }
+
+    // get fen notation of board
+    fen() {
+        let str = ""
+        for (let x in this.board) {
+            for (let y in this.board) {
+                str += y;
+            }
+            str += "/";
+        }
+    }
+}
